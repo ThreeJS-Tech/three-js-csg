8a9308d214ba17448b740e797d8793aa038627f5
THREE-TECH <65764022+ThreeJS-Tech@users.noreply.github.com>
2021-05-12 17:36:13 +0800

    add meshes

--- demo/src/mesh.js
+++ demo/src/mesh.js
@@ -1,17 +1,27 @@
 import THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 const ThreeBSP = require('../../index.js')(THREE);
 
 export const meshFactory = () => {
-  const box = new THREE.Mesh(new THREE.BoxGeometry(500, 100, 100));
-  const sphere = new THREE.Mesh(new THREE.SphereGeometry(100, 50, 50));
 
-  const sBSP = new ThreeBSP(sphere);
-  const bBSP = new ThreeBSP(box);
+  let loader = new STLLoader();
+  var box;
+  loader.load('yourMesh.stl', function (geo) {
+    box = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+    const sphere = new THREE.Mesh(new THREE.SphereGeometry(100, 50, 50));
 
-  const sub = bBSP.subtract(sBSP);
-  const newMesh = sub.toMesh();
+    const sBSP = new ThreeBSP(sphere);
+    const bBSP = new ThreeBSP(box);
 
-  newMesh.material = new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x1a1a1a, shininess: 30, shading: THREE.FlatShading });
+    const sub = bBSP.subtract(sBSP);
+    const newMesh = sub.toMesh();
 
-  return Object.assign({}, { csg: newMesh });
+    newMesh.material = new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x1a1a1a, shininess: 30, shading: THREE.FlatShading });
+
+    return Object.assign({}, { csg: newMesh });
+
+  })
+  // const box = new THREE.Mesh(new THREE.BoxGeometry(500, 100, 100));
+  // const box = 
+
 };
